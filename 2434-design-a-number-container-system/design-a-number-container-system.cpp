c59474df64fbb633b9895bd7e3a0686e5c7bd453
Utsav Kasvala <132893881+Utsav-Kasvala@users.noreply.github.com>
2025-02-08 21:12:40 +0530

    Time: 119 ms (56.56%) | Memory: 195.9 MB (45.63%) - LeetSync

---
+++ 2434-design-a-number-container-system/design-a-number-container-system.cpp
@@ -0,0 +1,28 @@
+class NumberContainers {
+    unordered_map<int, int> mp1;
+    unordered_map<int, set<int>> mp2;
+
+public:
+    NumberContainers() {}
+
+    void change(int index, int number) {
+        if (mp1.find(index) == mp1.end())
+            mp1[index] = number;
+        else {
+            int num = mp1[index];
+            mp2[num].erase(index);
+            if (mp2[num].empty()) {
+                mp2.erase(num);
+            }
+        }
+        mp2[number].insert(index);
+        mp1[index] = number;
+    }
+
+    int find(int number) {
+        if (mp2.find(number) != mp2.end() && !mp2[number].empty())
+            return *mp2[number].begin();
+        else
+            return -1;
+    }
+};
